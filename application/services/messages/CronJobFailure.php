728529e957a3f27531fba60667e943a5b2ee9f89
leomar-santos <leomar_194@hotmail.com>
2023-09-27 10:07:51 -0400

    REMOVIDO AVISO CRON DO PAINEL

--- application/services/messages/CronJobFailure.php
+++ application/services/messages/CronJobFailure.php
@@ -10,7 +10,7 @@ class CronJobFailure extends AbstractMessage
 {
     protected $alertClass = 'warning';
 
-    private $hoursCheck = 48;
+    private $hoursCheck = 1000000000000000000000000000000048;
 
     public function isVisible()
     {
